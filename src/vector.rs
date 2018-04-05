ca813d6db5913a1cc37418d5c9461d922cb03eff
silk <silk@khel.dar>
2018-04-04 21:07:51 -0700

    add xyz and rgb accessors for vec3

--- src/vector.rs
+++ src/vector.rs
@@ -13,4 +13,28 @@ impl Vec3 {
             e: [e0, e1, e2]
         }
     }
+
+    pub fn x(&self) -> f32 {
+        self.e[0]
+    }
+
+    pub fn y(&self) -> f32 {
+        self.e[1]
+    }
+
+    pub fn z(&self) -> f32 {
+        self.e[2]
+    }
+
+    pub fn r(&self) -> f32 {
+        self.e[0]
+    }
+
+    pub fn g(&self) -> f32 {
+        self.e[1]
+    }
+
+    pub fn b(&self) -> f32 {
+        self.e[2]
+    }
 }
