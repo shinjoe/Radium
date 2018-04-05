470600aea85d11d1f754c557fe2598744e4451bf
silk <silk@khel.dar>
2018-04-04 21:23:08 -0700

    implement the Add trait for Vec3, as well as from_int and from_float

--- src/vector.rs
+++ src/vector.rs
@@ -1,3 +1,5 @@
+use std::ops::Add;
+
 #[derive(Debug)]
 pub struct Vec3 {
     e: [f32; 3],
@@ -11,6 +13,18 @@ impl Vec3 {
     pub fn new(e0: f32, e1: f32, e2: f32) -> Vec3 {
         Vec3 {
             e: [e0, e1, e2]
+        }
+    }
+
+    pub fn from_float(arr: [f32; 3]) -> Vec3 {
+        Vec3 {
+            e: [arr[0], arr[1], arr[2]]
+        }
+    }
+
+    pub fn from_int(arr: [i32; 3]) -> Vec3 {
+        Vec3 {
+            e: [arr[0] as f32, arr[1] as f32, arr[2] as f32]
         }
     }
 
@@ -36,5 +50,15 @@ impl Vec3 {
 
     pub fn b(&self) -> f32 {
         self.e[2]
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            e: [self.e[0] + other.e[0], self.e[1] + other.e[1], self.e[2] + other.e[2]]
+        }
     }
 }
